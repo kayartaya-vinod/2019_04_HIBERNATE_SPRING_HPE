ffc53b16257aaf5bacafa777556a09e7e7899cdc
kayartaya-vinod <vinod@vinod.co>
2019-04-22 11:20:59 +0530

    🇮🇳

---
+++ EclipseWorkspace/training/src/main/java/training/entity/Person.java
@@ -0,0 +1,32 @@
+package training.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="persons")
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Double height;
+	@Column(name="dob")
+	private Date birthDate;
+}
+
+
+
+
+
